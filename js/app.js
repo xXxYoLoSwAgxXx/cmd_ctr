8a65baf97515d59a9ca8613f653856fdfa57d0b5
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2016-12-14 09:22:30 -1000

    faggot

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function changeba(){
+  document.getElementById('box').style.background="red";
+}
+function changebb(){
+  document.getElementById('box2').style.background="red";
+}
+function changebc(){
+  document.getElementById('box3').style.background="red";
+}
+function function_name() {
+  // body...
+}
